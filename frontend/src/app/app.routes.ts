3a1613b929c4fa0e377a24ac2df154bf6d4713c4
Lorena Heras <lhc15@gcloud.ua.es>
2024-11-25 16:07:47 +0100

    Nuevo componente chatbot creado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige a login por defecto
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'chatbot', component: ChatbotComponent },
 ];
