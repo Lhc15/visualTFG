78a323c0cbdb4385aa05301a6d8144fc3012feaf
lhc15 <lhc15@gcloud.ua.es>
2026-03-11 13:06:18 +0100

    componente charla

---
+++ frontend/src/app/conversamos/conversamos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversamos',
+  standalone: true,
+  imports: [],
+  templateUrl: './conversamos.component.html',
+  styleUrl: './conversamos.component.css'
+})
+export class ConversamosComponent {
+
+}
