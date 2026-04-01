fe9f31e7d87223d495203aac7f6f91926238e266
lhc15 <lhc15@gcloud.ua.es>
2026-04-01 14:33:07 +0200

    creación practica

---
+++ frontend/src/app/practica/practica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practica',
+  standalone: true,
+  imports: [],
+  templateUrl: './practica.component.html',
+  styleUrl: './practica.component.css'
+})
+export class PracticaComponent {
+
+}
